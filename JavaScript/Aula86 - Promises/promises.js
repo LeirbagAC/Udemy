4d5b1bf807847394ff08c57594673434bdd369ad
Gabriel Albuquerque Cabral <albuquerque373@gmail.com>
2025-02-07 15:26:32 -0300

    new file:   JavaScript/Aula86 - Promises/promises.js

---
+++ JavaScript/Aula86 - Promises/promises.js
@@ -0,0 +1,33 @@
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min ) + min)
+}
+
+function esperAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject('BAD Value');
+
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
+}
+
+esperAi('Frase 1', rand(1, 3))
+.then(resposta => {
+    console.log(resposta);
+    return esperAi('Frase 2',  rand(1, 3));
+})
+.then(resposta => {
+    console.log(resposta);
+    return esperAi(222,  rand(1, 3)); //Testando o reject com o '222'
+})
+.then(resposta => {
+    console.log(resposta);
+})
+.catch(e => {
+    console.log('ERRO: ', e);
+});
+
+console.log('Issso vai aparecer primeiro!');
