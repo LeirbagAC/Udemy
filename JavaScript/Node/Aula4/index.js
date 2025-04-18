a628c0faf17a6b21408377be7a2344d04d029059
Gabriel Albuquerque Cabral <albuquerque373@gmail.com>
2025-04-18 10:54:31 -0300

    Novos arquivos Node

--- JavaScript/Node/Aula4/index.js
+++ JavaScript/Node/Aula4/index.js
@@ -11,12 +11,16 @@ async function walk(files, rootDir) {
     for(let file of files) {
         const fileFullPath = path.resolve(rootDir, file);
         const stats = await fs.stat(fileFullPath);
-        console.log(file);
-
+        if(/\.git/g.test(fileFullPath)) continue;
+        if(/node_modules/g.test(fileFullPath)) continue;
+        
         if(stats.isDirectory()) {
             readdir(fileFullPath);
             continue;
         }
+        
+        if(!/\.css/g.test(fileFullPath)) continue;
+        console.log(fileFullPath);
 
     }
 }
